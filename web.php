40d7e31d76a678612529344f5b14775a65e23193
waizhi <103483506+waizhi@users.noreply.github.com>
2022-06-13 14:51:39 +0900

    Add files via upload

---
+++ web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/building', function() {
+    return'建物です';
+});
+Route::get('/building/{room}',function($room){
+    return '部屋番号は'.$room.'です';
+});
